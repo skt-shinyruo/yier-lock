fd98ec3129c4b69ab1b5b2367cec2420b1af5255
bubu <1654388696@qq.com>
2026-03-22 19:57:31 +0800

    build: rebuild benchmark module for lock 2.0

---
+++ distributed-lock-benchmarks/README.md
@@ -0,0 +1,13 @@
+# Distributed Lock Benchmarks
+
+## Prerequisites
+
+- Docker
+- local 2.0 artifacts installed with `mvn -q install -DskipTests`
+
+## Commands
+
+- compile
+- smoke test
+- package
+- run one benchmark
---
+++ distributed-lock-benchmarks/src/main/java/com/mycorp/distributedlock/benchmarks/RuntimeLifecycleBenchmark.java
@@ -0,0 +1,8 @@
+package com.mycorp.distributedlock.benchmarks;
+
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class RuntimeLifecycleBenchmark {
+}
